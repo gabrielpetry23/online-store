29c72c2e7018cb6a91b0f902319df642637f0215
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-05-29 14:52:51 -0300

    loadSpinner and orderImages added

---
+++ admin-panel/components/LoadSpinner.js
@@ -0,0 +1,10 @@
+import { BeatLoader } from "react-spinners";
+
+export default function LoadSpinner() {
+    return (
+        <BeatLoader
+            color={'#1E3A8A'}
+            speedMultiplier={2}
+        ></BeatLoader>
+    )
+}
--- admin-panel/components/ProductForm.js
+++ admin-panel/components/ProductForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import LoadSpinner from './LoadSpinner';
+import { ReactSortable } from 'react-sortablejs';
 
 export default function ProductForm({
     _id,
@@ -14,6 +16,7 @@ export default function ProductForm({
     const [price, setPrice] = useState(existingPrice || '');
     const [goToProducts, setGoToProducts] = useState(false);
     const [images, setImages] = useState(existingImages || []);
+    const [isUploading, setIsUploading] = useState(false);
     const router = useRouter();
 
     async function saveProduct(e) {
@@ -38,8 +41,11 @@ export default function ProductForm({
 
     async function uploadImg(e) {
         const files = e.target?.files;
+        
         if (files.length > 0) {
+            setIsUploading(true);
             const data = new FormData();
+
             for (const file of files) {
                 data.append('file', file);
             }
@@ -47,7 +53,12 @@ export default function ProductForm({
             setImages(oldImages => {
                 return [...oldImages, ...res.data.links];
             });
+            setIsUploading(false);
         }
+    }
+
+    function updateImgOrder(images) {
+        setImages(images);
     }
 
     return (
@@ -64,12 +75,22 @@ export default function ProductForm({
                     Photos
                 </label>
                 <div className='mb-2 flex flex-wrap gap-2'>
-                    {!!images?.length && images.map(link => (
-                        <div key={link} className='inline-block h-24'>
-                            <img src={link} alt="" className='rounded-lg' />
+                    <ReactSortable
+                        list={images}
+                        className='flex flex-wrap gap-2'
+                        setList={updateImgOrder}>
+                        {!!images?.length && images.map(link => (
+                            <div key={link} className='h-24'>
+                                <img src={link} alt="" className='rounded-lg' />
+                            </div>
+                        ))}
+                    </ReactSortable>
+                    {isUploading && (
+                        <div className='h-24 flex items-center'>
+                            <LoadSpinner></LoadSpinner>
                         </div>
-                    ))}
-                    <label className='inline-block w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200'>
+                    )}
+                    <label className='w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                         </svg>
@@ -78,9 +99,6 @@ export default function ProductForm({
                         </div>
                         <input type="file" onChange={uploadImg} className='hidden'/>
                     </label>
-                    {!images?.length && (
-                        <div>This product dont have photos</div>
-                    )}
                 </div>
 
                 <label>Description</label>
