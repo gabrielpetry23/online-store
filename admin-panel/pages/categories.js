96ca1b14e98e77e06012674d3e428c8174e519fb
gabrielpetry23 <gabriel.petry23@gmail.com>
2024-06-13 17:39:25 -0300

    categories working

--- admin-panel/pages/categories.js
+++ admin-panel/pages/categories.js
@@ -1,14 +1,26 @@
 import Layout from "@/components/Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Categories() {
-const [name, setName] = useState('');
+    const [name, setName] = useState('');
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+    fetchCategories();
+    }, []);
+
+    function fetchCategories() {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        });
+    }
 
     async function saveCategory(e) {
         e.preventDefault();
         await axios.post('/api/categories', {name});
         setName('');
+        fetchCategories();
     }
     return (
         <Layout>
@@ -26,6 +38,20 @@ const [name, setName] = useState('');
                     Save
                 </button>
             </form>
+            <table className="basic mt-4">
+                <thead>
+                    <tr>
+                        <td>Category name</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {categories.length > 0 && categories.map(category => (
+                        <tr>
+                            <td>{category.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
     );
 }
